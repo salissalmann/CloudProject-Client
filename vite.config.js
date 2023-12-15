aa5df579f324fead19449f1451467232f9797fd8
salissalmann <i200489@nu.edu.pk>
2023-12-15 13:55:47 +0500

    Nginx Proxy added

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+//add nginx proxy
+const proxy = {
+  '/api': {
+    target: 'http://localhost:8080',
+    changeOrigin: true,
+    rewrite: (path) => path.replace(/^\/api/, ''),
+  },
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy,
+  },
 })
